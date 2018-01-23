c44c3498e37c93973ed7f123c5d7495353cea242
IgniparousTempest <cr.pitcher@gmail.com>
2018-01-23 13:38:10 +0200

    Added better home page, LED status light, and restart on disconnect.

--- hass-water-pump.ino
+++ hass-water-pump.ino
@@ -14,18 +14,22 @@ MDNSResponder mdns;
 ESP8266WebServer server(80);
 //----------------------------for local web
 
-int pinout=2;  //for relay, and internal led
-int pinsw=0;  //for switch
+int pin_sw=2;  //for switch (D4)
+int pin_pump=4;  //for pump (D2)
+int pin_led=5;  //for status LED (D1)
 boolean pump_status;
 boolean sw_status;
 Ticker timer; // Timer to switch off pump
 
 void setup() {
-  pinMode(pinout, OUTPUT);
-  pinMode(pinsw, INPUT);
+  pinMode(pin_sw, INPUT);
+  pinMode(pin_pump, OUTPUT);
+  pinMode(pin_led, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(115200);
   Serial.println("Booting");
-   WiFi.mode(WIFI_STA);
+  WiFi.hostname("HASS_Water_Pump");
+  WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   while (WiFi.waitForConnectResult() != WL_CONNECTED) {
     Serial.println("Connection Failed! Rebooting...");
@@ -58,11 +62,16 @@ void setup() {
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 
-  if (mdns.begin("esp8266", WiFi.localIP())) 
+  if (mdns.begin("esp8266", WiFi.localIP()))
     Serial.println("MDNS responder started");
  
   server.on("/", [](){
     server.send(200, "text/html", home_page());
+  });
+  // For debug only
+  server.on("/toggle", HTTP_GET, [](){
+    pump_toggle();
+    server.send(200, "text/json", response_json());
   });
   server.on("/pump", HTTP_GET, [](){
     server.send(200, "text/json", response_json());
@@ -70,6 +79,7 @@ void setup() {
   server.on("/pump", HTTP_POST, [](){
     StaticJsonBuffer<200> request_buffer;
     JsonObject& request = request_buffer.parseObject(server.arg("plain"));
+
     boolean desired_light_state = request["active"];
     if (desired_light_state) {
       int duration = request["duration"];
@@ -81,7 +91,9 @@ void setup() {
   });
 
   server.begin();
-  //-------------------------------------------add your code here
+  
+  //initialise pump
+  pump_off();
 }
 
 String response_json() {
@@ -90,12 +102,37 @@ String response_json() {
 }
 
 String home_page() {
-    String status_text = pump_status ? "<span style='color: green'>pumping</span>" : "<span style='color: orange'>idle</span>";
-    return "<h1>ESP8266 Web Server Pump Switch</h1>" +
-           "<p>Pump is " + status_text + "</p>" +
-           "<form action='' method='post'>" +
-           "  <button name='foo' value='upvote'>Toggle Pump</button>" +
-           "</form>";
+    String status_text = pump_status ? "<span style='color: green; font-weight: bold;'>pumping</span>" : "<span style='color: orange; font-weight: bold;'>idle</span>";
+    String data_text = pump_status ? "{\"active\": true, \"duration\": 600}" : "{\"active\": false}";
+    String html = "<!DOCTYPE HTML>\n";
+    html += "<html>\n";
+    html += "  <head>\n";
+    html += "    <title>HASS Water Pump</title>\n";
+    html += "    <script src='https://code.jquery.com/jquery-3.3.1.min.js' integrity='sha256-FgpCb/KJQlLNfOu91ta32oNMZxltwRo8QtmkMRdAu9=' crossorigin='anonymous'></script>\n";
+    html += "    <script>\n";
+    html += "      $(function () {\n";
+    html += "        $('form').on('submit', function (e) {\n";
+    html += "          e.preventDefault();\n";
+    html += "          $.post({\n";
+    html += "            url: '/pump',\n";
+    html += "            data: '" + data_text + "',\n";
+    html += "            success: function () {\n";
+    html += "              location.reload();\n";
+    html += "            }\n";
+    html += "          });\n";
+    html += "        });\n";
+    html += "      });\n";
+    html += "    </script>\n";
+    html += "  </head>\n";
+    html += "  <body>\n";
+    html += "    <h1>ESP8266 Web Server Water Pump</h1>\n";
+    html += "    <p>Pump is " + status_text + "</p>\n";
+    html += "    <form>\n";
+    html += "      <button name='submit' value='submit'>Toggle Pump</button>\n";
+    html += "    </form>\n";
+    html += "  </body>\n";
+    html += "</html>";
+    return html;
 }
 
 void pump_toggle() {
@@ -113,7 +150,9 @@ void pump_toggle() {
 void pump_off() {
     Serial.println("Turning pump off");
     pump_status = false;
-    digitalWrite(pinout, LOW);
+    digitalWrite(LED_BUILTIN, HIGH);
+    digitalWrite(pin_led, LOW);
+    digitalWrite(pin_pump, LOW);
     timer.detach();
 }
 
@@ -122,19 +161,25 @@ void pump_off() {
  * @param duration_in_seconds The duration to turn the pump on for.
  */
 void pump_on(int duration_in_seconds) {
-    Serial.println("Turning pump on");
+    Serial.println("Turning pump on for " + String(duration_in_seconds) + " seconds");
     pump_status = true;
-    digitalWrite(pinout, HIGH);
+    digitalWrite(LED_BUILTIN, LOW);
+    digitalWrite(pin_led, HIGH);
+    digitalWrite(pin_pump, HIGH);
     timer.once(duration_in_seconds, pump_off);
 }
 
 void loop() {
   ArduinoOTA.handle();
-  //-------------------------------------------add your code here
   server.handleClient();
 
+  // Restart device if WiFi is disconnected and it is not pumping
+  // TODO: Is there not a onDisconnected handler that can do this better?
+  if (WiFi.status() == WL_DISCONNECTED && !pump_status)
+     ESP.restart();
+
   //TODO: Use button to toggle pump state
+
   delay(200);
-  
 }
 
